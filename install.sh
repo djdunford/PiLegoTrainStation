e76c00715765f5babe0fd8c1a48ca0e5e6fcd3f5
ddunford <djdunford@gmail.com>
2019-01-04 22:10:28 +0000

    revised installation and service script files

--- install.sh
+++ install.sh
@@ -9,21 +9,21 @@
 #
 
 # script control variables
-appdir="/opt/xmastrain"
+appdir="/opt/signals"
 
 # update package manager
 apt-get update
 
 # install required modules
-apt-get install -y python3-pip
-pip3 install RPLCD python3-smbus
+apt-get install -y python3-pip python3-smbus
+pip3 install RPLCD
 
 # create directory structure for app and webapp
 mkdir -p $appdir
 chgrp -R pi $appdir
 
 # copy webapp and wsgi files
-cp xmastrain.py $appdir
+cp onesignal.py $appdir
 
 # install service file
 cp signals.service /lib/systemd/system
