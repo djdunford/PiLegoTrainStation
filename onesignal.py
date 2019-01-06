1fa94b775c64400a18e0b4c622b7131d5bbc9e93
ddunford <djdunford@gmail.com>
2019-01-06 21:55:40 +0000

    changed setting for auto linebreaks

--- onesignal.py
+++ onesignal.py
@@ -34,7 +34,7 @@ import time
 GPIO.setmode(GPIO.BCM)
 
 # set up display
-lcd = CharLCD(i2c_expander='PCF8574',address=0x27,port=1,cols=20,rows=4,dotsize=8,charmap='A02',auto_linebreaks=False,backlight_enabled=True)
+lcd = CharLCD(i2c_expander='PCF8574',address=0x27,port=1,cols=20,rows=4,dotsize=8,charmap='A02',auto_linebreaks=True,backlight_enabled=True)
 
 # set up station list and initial departures
 stations = ['Petersfield','King\'s Cross','Worcester','Royston','Hitchin','Letchworth','Bristol','Oxford','Cardiff','Baldock','Stevenage','Welwyn','Finsbury Pk','Blackfriars','Brighton','St. Pancras','Knebworth']
