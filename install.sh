0bf652382b62c9ffccbb790580b0972f17452dea
djdunford <45966937+djdunford@users.noreply.github.com>
2019-02-11 22:53:18 +0000

    Updated source comments and licencing (#4)
    * updated source code comments, does not enable on startup
    
    * Create LICENSE
    
    * added licence text to source

--- install.sh
+++ install.sh
@@ -2,11 +2,33 @@
 #
 # install.sh
 #
-# install the service and the WSGI web app on
-# Raspbian Jessie for XmasTrain 2017
+# install the service to run on startup
 #
 # by Darren Dunford
 #
+
+# MIT License
+# 
+# Copyright (c) 2019 Darren Dunford
+# 
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
 
 # script control variables
 appdir="/opt/signals"
@@ -29,7 +51,6 @@ cp onesignal.py $appdir
 cp signals.service /lib/systemd/system
 chmod 644 /lib/systemd/system/signals.service
 
-# restart Apache and xmastrain service
+# restart Apache and signals service
 systemctl daemon-reload
 systemctl restart signals
-systemctl enable signals
